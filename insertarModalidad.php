2b0e590031bb44021bd8e868107f0791d9420b35
MaryuriZavala <45839486+MaryuriZavala@users.noreply.github.com>
2018-12-12 23:26:32 -0600

    Update insertarModalidad.php

--- insertarModalidad.php
+++ insertarModalidad.php
@@ -9,6 +9,7 @@ if (isset($_SESSION["ses_id"])) {
   window.location= "index.php";
   </script>';
 };
+if($_SESSION['nivelAcceso'] == 3){
 if (isset($_POST['guardar'])){
   $obj= new funcionesBD();
 
@@ -226,4 +227,12 @@ if (isset($_POST['guardar'])){
   if (isset($notifyVerification)) {
     echo $obj->notify($notifyVerification[0],$notifyVerification[1]);
   }
+}
+else{
+   echo '<script>
+  alert("No Tienes acceso a esta pagina");
+   window.location= "home.php";
+
+  </script>';
+}
   include_once('layouts/footer.php'); ?>
