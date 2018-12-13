86584fbb1ae70e4f7768ec85ef22b3a3d31fcfd2
MaryuriZavala <45839486+MaryuriZavala@users.noreply.github.com>
2018-12-12 23:05:27 -0600

    Update insertarAsignatura.php

--- insertarAsignatura.php
+++ insertarAsignatura.php
@@ -9,6 +9,7 @@ if (isset($_SESSION["ses_id"])) {
   window.location= "index.php";
   </script>';
 };
+if($_SESSION['nivelAcceso'] == 3){
   if (isset($_POST['guardar'])){
 
     for ($i=1; $i <= $_POST['cantAsignaturas'] ; $i++) {
@@ -169,5 +170,13 @@ if (isset($_SESSION["ses_id"])) {
   if (isset($notifyVerification)) {
     echo $obj->notify($notifyVerification[0],$notifyVerification[1]);
   }
+}
+else{
+   echo '<script>
+  alert("No Tienes acceso a esta pagina");
+   window.location= "home.php";
+
+  </script>';
+}
 
   include_once('layouts/footer.php'); ?>
